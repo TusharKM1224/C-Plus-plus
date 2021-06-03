c7d6553ddad00b19551757e9babef65723b932b4
TusharKM1224 <75167909+TusharKM1224@users.noreply.github.com>
2021-06-04 00:59:50 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+//TO PRINT GREETINGS AGAINST YOUR NAME!!
+using namespace std;
+int main()
+{
+    char n[20];
+    cout << "enter your name";
+    cin >> n;
+    cout << endl;
+    cout << "Good Moring " << n << endl;
+
+    return 0;
+}
